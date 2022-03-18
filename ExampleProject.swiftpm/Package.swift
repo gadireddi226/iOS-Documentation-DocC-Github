64b7c01eb565d93fdba61a96fe0fcdb40ec8dea1
Erik Gadireddi <erik.gadireddi@avast.com>
2022-03-18 17:21:59 +0100

    Modify Package.swift to not fail via comandline

--- ExampleProject.swiftpm/Package.swift
+++ ExampleProject.swiftpm/Package.swift
@@ -5,35 +5,46 @@
 // Do not edit it by hand because the contents will be replaced.
 
 import PackageDescription
-import AppleProductTypes
+
+#if canImport(AppleProductTypes)
+    import AppleProductTypes
+
+    let products: [Product] = [
+                .iOSApplication(
+                    name: "ExampleProject",
+                    targets: ["ExampleProject"],
+                    displayVersion: "1.0",
+                    bundleVersion: "1",
+                    appIcon: .asset("AppIcon"),
+                    supportedDeviceFamilies: [
+                        .pad,
+                        .phone
+                    ],
+                    supportedInterfaceOrientations: [
+                        .portrait,
+                        .landscapeRight,
+                        .landscapeLeft,
+                        .portraitUpsideDown(.when(deviceFamilies: [.pad]))
+                    ]
+                )
+    ]
+#else
+    let products: [Product] = []
+#endif
 
 let package = Package(
     name: "ExampleProject",
     platforms: [
-        .iOS("15.2")
+        .iOS("15.2"),
+        .macOS("11.0")
     ],
-    products: [
-        .iOSApplication(
-            name: "ExampleProject",
-            targets: ["App"],
-            displayVersion: "1.0",
-            bundleVersion: "1",
-            appIcon: .asset("AppIcon"),
-            supportedDeviceFamilies: [
-                .pad,
-                .phone
-            ],
-            supportedInterfaceOrientations: [
-                .portrait,
-                .landscapeRight,
-                .landscapeLeft,
-                .portraitUpsideDown(.when(deviceFamilies: [.pad]))
-            ]
-        )
+    products: products,
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main")
     ],
     targets: [
         .executableTarget(
-            name: "App",
+            name: "ExampleProject",
             path: "App"
         )
     ]
